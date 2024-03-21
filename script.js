5ed0e9bf1a674e226759ec1b9f44766fb0db4079
Jurre van Eijk <jurreveijk13@gmail.com>
2024-03-21 13:52:29 +0100

    working on this shit

--- script.js
+++ script.js
@@ -1,24 +1,70 @@
-const submitbutton = document.getElementById("submitbutton");
 const email_input = document.getElementById("email");
-const country = document.getElementById("country");
-const zip_code = document.getElementById("zip-code");
-const password = document.getElementById("password");
-const password_confirmation = document.getElementById("password-confirmation");
-const error = document.getElementById("error1");
+const country_input = document.getElementById("country");
+const zip_code_input = document.getElementById("zip-code");
+const password_input = document.getElementById("password");
+const password_confirmation_input = document.getElementById(
+	"password-confirmation"
+);
 
-function validateForm() {}
+const email_error = document.querySelector(".email-error");
+const country_error = document.querySelector(".country-error");
+const zip_error = document.querySelector(".zip-error");
+const password_error = document.querySelector(".password-error");
+const pwc_error = document.querySelector(".pwc-error");
+
 function validateEmail() {
-  email_input.addEventListener("input", () => {
-    if (!email_input.validity.valid) {
-      if (email_input.validity.typeMismatch) {
-        email_input.classList.add("error");
-      }
-      email_input.classList.add("error");
-    } else {
-      errorMessage.textContent = "";
-      email_input.classList.remove("error");
-    }
-  });
+	if (!email_input.validity.valid) {
+		email_input.classList.add("error");
+		email_error.classList.add("show");
+		email_error.textContent = "Please enter a valid email address.";
+	} else {
+		email_input.classList.remove("error");
+		email_error.textContent = "";
+		email_error.classList.remove("show");
+		email_input.classList.add("good");
+	}
 }
 
-validateEmail();
+function validateCountry() {
+	if (!country_input.validity.valid) {
+		if (country_input.validity.valueMissing) {
+			country_error.textContent = "Please enter a country.";
+		} else if (country_input.validity.tooShort) {
+			country_error.textContent = "No countries with less than 4 characters.";
+		} else if (country_input.validity.tooLong) {
+			country_error.textContent = "No countries with more than 56 characters.";
+		}
+		country_input.classList.add("error");
+		country_error.classList.add("show");
+	} else {
+		country_input.classList.remove("error");
+		country_error.textContent = "";
+		country_error.classList.remove("show");
+		country_input.classList.add("good");
+	}
+}
+
+function validateZipCode() {
+	if (!zip_code_input.validity.valid) {
+		if (zip_code_input.validity.valueMissing) {
+			zip_error.textContent = "Please enter a zip code.";
+		} else {
+			zip_error.textContent = "Please enter a correct zip code.";
+		}
+		zip_code_input.classList.add("error");
+		zip_error.classList.add("show");
+	} else {
+		zip_code_input.classList.remove("error");
+		zip_error.textContent = "";
+		zip_error.classList.remove("show");
+		zip_code_input.classList.add("good");
+	}
+}
+
+function validateForm() {
+	email_input.addEventListener("input", validateEmail);
+	country_input.addEventListener("input", validateCountry);
+	zip_code_input.addEventListener("input", validateZipCode);
+}
+
+validateForm();
